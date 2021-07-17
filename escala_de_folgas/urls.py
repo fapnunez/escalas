a2e4e22d9b45c35b7228bb6eb461781924addaa1
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-07-16 14:41:46 -1200

    sistema

--- escala_de_folgas/urls.py
+++ escala_de_folgas/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('sistema/', include('core.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
